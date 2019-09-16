1ab1a3b78b7637290c0d25b1b8892ac112e93e49
CheezItMan <chris@adadevelopersacademy.org>
2019-09-15 23:51:35 -0700

    added sort_by_length tests

---
+++ test/sort_by_length.rb
@@ -0,0 +1,15 @@
+require_relative "test_helper"
+
+describe "sort_by_length" do
+  it "will return an empty array for an empty string" do
+    expect(sort_by_length("")).must_equal []
+  end
+  
+  it "will return an array of words, by length" do
+    expect(sort_by_length("I love Ada")).must_equal ["I", "Ada", "love"]
+  end
+
+  it "will return an array of words by length, words that are of equal length will appear in the order they appear" do
+    expect(sort_by_length("words of equal length")).must_equal ["of", "words", "equal", "length"]
+  end
+end
