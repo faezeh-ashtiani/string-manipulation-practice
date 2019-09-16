89f7780de1066848dbea4e8dbb8378f5c84180ce
CheezItMan <chris@adadevelopersacademy.org>
2019-09-15 23:51:20 -0700

    added sort_by_length method

---
+++ lib/sort_by_length.rb
@@ -0,0 +1,7 @@
+# A method which will return an array of the words in the string
+#  sorted by the length of the word.
+# Time complexity: ?
+# Space complexity: ?
+def sort_by_length(my_sentence)
+  raise NotImplementedError
+end
