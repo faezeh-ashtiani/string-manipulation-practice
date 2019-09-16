daa1475541497693afd80d30e36b30a419913e4d
CheezItMan <chris@adadevelopersacademy.org>
2019-09-16 00:29:13 -0700

    add another test

--- test/sort_by_length.rb
+++ test/sort_by_length.rb
@@ -12,4 +12,8 @@ describe "sort_by_length" do
   it "will return an array of words by length, words that are of equal length will appear in the order they appear" do
     expect(sort_by_length("words of equal length")).must_equal ["of", "words", "equal", "length"]
   end
+
+  it "will return an array of words by length, words that are of equal length will appear in the order they appear" do
+    expect(sort_by_length("I love great awesome words")).must_equal ["I", "love", "great", "words", "awesome"]
+  end
 end
